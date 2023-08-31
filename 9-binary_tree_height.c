ce81ca7782518b58c70fbb3f7ee6fece4cdb95b2
root <sackitey68@gmail.com>
2023-08-31 11:53:35 -0700

    Task 9 Fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-		return ((l > r) ? l : r)
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
