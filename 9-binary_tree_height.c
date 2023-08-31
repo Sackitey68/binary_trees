c804e5a64a16b6708bd8ca4324bc0220ba937e35
root <sackitey68@gmail.com>
2023-08-31 11:42:43 -0700

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,11 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l = 0, k = 0;
+		size_t l = 0, r = 0;
 
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		k = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l > k) ? l : k)
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((l > r) ? l : r)
 	}
 	return (0);
 }
